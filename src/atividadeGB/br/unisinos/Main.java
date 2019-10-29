bbe019ecd7ec0225227362f9a41be598bd555a83
Patrick Hammes <56709367+patrickhammes@users.noreply.github.com>
2019-10-29 16:30:24 -0300

    Update Main.java

--- src/atividadeGB/br/unisinos/Main.java
+++ src/atividadeGB/br/unisinos/Main.java
@@ -3,7 +3,19 @@ package atividadeGB.br.unisinos;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//instanciando a SimpleFactory
+		SimpleFactory sf = new SimpleFactory();
+		
+       		//utilizando a SimpleFactory para instanciar as classes
+		Compra compra = sf.createCompra();
+        	Pagamento pag = sf.createPagamento();
+        	Entrega entrega = sf.createEntrega();
+		
+		//adicionar item carrinho
+		
+		//escolher metodo pagamento
+		
+		//finalizar compra
 	}
 
 }
