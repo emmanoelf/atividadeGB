22a6026ea7b02f421b3ba3a5a7b557128e17c2c0
Patrick Hammes <56709367+patrickhammes@users.noreply.github.com>
2019-10-29 17:26:46 -0300

    Update Compra.java

--- src/atividadeGB/br/unisinos/Compra.java
+++ src/atividadeGB/br/unisinos/Compra.java
@@ -1,7 +1,26 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 class Compra{
+    private static final AtomicInteger count = new AtomicInteger(0);
+    private final int id;
+    private String status = "Em Andamento";
 
     public Compra(){
-        
+        this.id = count.incrementAndGet();
     }
     
+    public Integer getId(){
+        return this.id;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+    public boolean isOK(){
+        if(this.status.equals("Finalizado")){
+            return true;
+        }
+        return false;
+    }
 }
