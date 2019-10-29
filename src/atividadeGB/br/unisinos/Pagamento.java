6d0ae663a92c8dd02eeb69368531ccfd9e332673
Patrick Hammes <56709367+patrickhammes@users.noreply.github.com>
2019-10-29 17:27:33 -0300

    Update Pagamento.java

--- src/atividadeGB/br/unisinos/Pagamento.java
+++ src/atividadeGB/br/unisinos/Pagamento.java
@@ -1,7 +1,21 @@
 class Pagamento {
-
+    private String status;
 
     public Pagamento(){
-        
+    }
+
+    public void validarPagamento(){
+        if(true){
+            setStatus("Pagamento Aprovado");
+        }
+        setStatus("Erro no pagamento");
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+    
+    public String getStatus(){
+        return this.status;
     }
 }
