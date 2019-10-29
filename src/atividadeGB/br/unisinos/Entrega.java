b7416ee16bc99e5f373ddc14378dc6fabdb1d58c
Patrick Hammes <56709367+patrickhammes@users.noreply.github.com>
2019-10-29 17:27:13 -0300

    Update Entrega.java

--- src/atividadeGB/br/unisinos/Entrega.java
+++ src/atividadeGB/br/unisinos/Entrega.java
@@ -1,7 +1,19 @@
 class Entrega{
+    private String tipoEntrega;
 
     public Entrega(){
-
+        this.tipoEntrega = "Padrão";
     }
 
+    public void setTipoEntrega(String tipo){
+        this.tipoEntrega = tipo;
+    }
+
+    public String getTipoEntrega(){
+        return this.tipoEntrega;
+    }
+
+    public void adicionarFilaDeEnvio(Compra compra){
+        System.out.println("Pedido ID: "+compra.getId()+" encaminhado para fila de entrega");
+    }
 }
